b6c410fd8e83eb2bcd7eab095e9ec89e4a33b8b2
Mohamed Haidar <mhaidar@squiz.com.au>
2019-02-15 01:39:06 +0000

    The codes are namespaced with the lower case package name, rather than the class

--- src/Storage/StorageFactory.php
+++ src/Storage/StorageFactory.php
@@ -48,7 +48,7 @@ class StorageFactory
     public static function getDataStore(string $storeid)
     {
         $namespace = str_replace('\Framework\StorageFactory', '', static::class);
-        $namespace = str_replace('\\', '/', $namespace);
+        $namespace = strtolower(str_replace('\\', '/', $namespace));
         $storeCode = $namespace.'/'.$storeid;
 
         if (isset(self::$stores['data'][$storeCode]) === true) {
@@ -75,7 +75,7 @@ class StorageFactory
     public static function getUserStore(string $storeid)
     {
         $namespace = str_replace('\Framework\StorageFactory', '', static::class);
-        $namespace = str_replace('\\', '/', $namespace);
+        $namespace = strtolower(str_replace('\\', '/', $namespace));
         $storeCode = $namespace.'/'.$storeid;
 
         if (isset(self::$stores['user'][$storeCode]) === true) {
